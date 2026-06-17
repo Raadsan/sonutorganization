a4b52a062dd2ea0e09b99983ca67969b77a91925
ayubabdi254 <abdullahiayuub235@gmail.com>
2026-06-17 13:20:19 +0300

    first commit

---
+++ app/Resources/ournews/page.tsx
@@ -0,0 +1,7 @@
+export default function ournews() {
+  return (
+    <div>
+      <h1>ournews</h1>
+    </div>
+  );
+}
---
+++ app/estatemembers/page.tsx
@@ -0,0 +1,7 @@
+export default function estateMembers() {
+  return (
+    <div>
+      <h1>estateMembers</h1>
+    </div>
+  );
+}
